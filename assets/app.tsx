2d7c977317c5170fc52a55c3b4d1860bfcb29c27
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2025-05-24 02:42:20 -0500

    react base

---
+++ assets/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import '@styles/app.scss';
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <h1>Calendario</h1>
+    </StrictMode>
+);
